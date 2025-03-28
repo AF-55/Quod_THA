0705ec5bb59f28d3b1cf5abff5d4887767d1f5a8
Aaron Fargeon <aaron.fargeon@eleves.enpc.fr>
2025-03-28 11:18:13 +0100

    changed histogram color

--- transactions_customer_hist.py
+++ transactions_customer_hist.py
@@ -5,9 +5,9 @@ df = pd.read_csv("data/all_transactions.csv", index_col=0)
 
 customer_monthly = df.groupby(['customer_id', 'year_month']).size().reset_index(name='transactions')
 customer_counts = customer_monthly.groupby('customer_id')['transactions'].sum().sort_values(ascending=False)
-plt.figure(figsize=(12, 8))
-plt.bar(customer_counts.index.astype(str), customer_counts.values, width=1)
 
+plt.figure(figsize=(12, 8))
+plt.bar(customer_counts.index.astype(str), customer_counts.values, width=1, color='red')
 plt.xlabel("Customers from best to worst buyer")
 plt.ylabel("Number of Transactions")
 plt.title("Transactions per Customer (Descending Order)")
